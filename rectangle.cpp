4f00e976e1c69c0a3dc55e69f5a4a7a7400b1409
Christian Fobel <christian@fobel.net>
2011-07-29 09:33:25 -0400

    Add stroke to rectangle.

--- rectangle.cpp
+++ rectangle.cpp
@@ -3,16 +3,35 @@
 class Window : public Gtk::Window {
   private:
     Gtk::DrawingArea area;
+    Gtk::Allocation allocation;
+    Cairo::RefPtr<Cairo::Context> context;
+    int width;
+    int height;
+
+    void draw_block(double x_from, double y_from, double x_to, double y_to) {
+        context->save();
+        context->rectangle(x_from, y_from, x_to, y_to);
+        context->set_source_rgba(0, 0.8, 0, 1);
+        context->fill_preserve();
+        context->restore();
+        context->save();
+        context->set_line_width(0.01);
+        context->stroke();
+        context->restore();
+    }
 
     bool on_area_expose(GdkEventExpose* event) {
-        Gtk::Allocation allocation = area.get_allocation();
-        Cairo::RefPtr<Cairo::Context> context =
-            area.get_window()->create_cairo_context();
-        int width = allocation.get_width();
-        int height = allocation.get_height();
-        context->set_source_rgba(0, 0, 0, 1);
-        context->rectangle(0, 0, double(width)/10, double(height)/10);
-        context->fill();
+        allocation = area.get_allocation();
+        context = area.get_window()->create_cairo_context();
+        width = allocation.get_width();
+        height = allocation.get_height();
+
+        context->scale(width, height);
+
+        context->save();
+        draw_block(0, 0, 0.1, 0.1);
+        draw_block(0.5, 0.5, 0.9, 0.9);
+        context->restore();
         return true;
     }
 
